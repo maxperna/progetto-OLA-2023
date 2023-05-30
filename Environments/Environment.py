8606011d41d9e41358bbe80881d00ffca7511a29
maxperna <maxperna@users.noreply.github.com>
2023-05-30 15:22:19 +0200

    Project beginning, definition of class skeletons Environment, Users, Product

---
+++ Environments/Environment.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+class Environment():
+    def __init__(self):
+        self.n_rounds = 365   #365 day long
---
+++ Environments/Users.py
@@ -0,0 +1,52 @@
+from abc import ABC
+import numpy as np
+
+
+
+
+class User(ABC):
+    """
+    Abstract class used to define User
+    :f1: value of binary feature F1 for specific class
+    :f2: value of binary feature F2 for specific class
+    :prices: set of available prices for class of user
+    """
+
+    def __init__(self, f1_value, f2_value):
+        self.f1_value = f1_value
+        self.f2_value = f2_value
+
+    def draw_price(self, p):
+        np.choiche
+
+
+class UserC1(User):
+    """
+    User class C1
+    Frequent User
+    """
+
+    def __init__(self):
+        super().__init__(True, True)
+
+
+class UserC2(User):
+    """
+    User class C2
+    Non Member User
+    """
+
+    def __init__(self):
+        super().__init__(False, False)
+
+
+class UserC3(User):
+    """
+    User class C3
+    Occasional User
+    """
+
+    def __init__(self):
+        feature1 = np.random.choice([True, False])
+        feature2 = not feature1
+        super().__init__(feature1, feature2)
