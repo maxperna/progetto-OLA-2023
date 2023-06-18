bbabd42353dde9f19d4b5dfc3bc0d1c0365d7b22
MLiuk <92759081+marcolucchini@users.noreply.github.com>
2023-06-18 18:54:24 +0200

    Step 2 first implementation

--- Learners/UCB1_Learner.py
+++ Learners/UCB1_Learner.py
@@ -27,5 +27,5 @@ class UCB1_Learner(Learner):
     def update(self, pulled_arm, reward, price):
         self.t += 1
         self.n_pulls[pulled_arm] += 1
-        self.update_observation(pulled_arm, reward*price)       # update with reward*price
+        self.update_observations(pulled_arm, reward*price)       # update with reward*price
         self.expected_rewards[pulled_arm] = (self.expected_rewards[pulled_arm] * (self.t - 1) + reward * price) / self.t
