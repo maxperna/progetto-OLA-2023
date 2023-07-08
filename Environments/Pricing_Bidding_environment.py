a9d720976e0853ea3484f3c859540bf2e224f7c1
chiaramocetti <chiara.mocetti@gmail.com>
2023-07-08 20:04:30 +0200

    Pricing Bidding Environment

--- Environments/Pricing_Bidding_environment.py
+++ Environments/Pricing_Bidding_environment.py
@@ -9,8 +9,10 @@ class PricingBiddingEnvironment():
         # Assignments and Initializations
         self.actions = actions
         step=5
-        for i in range(1,49,1):
-            self.means[0:step*i] = user.demand_curve(user.prices) + user.click_vs_bid(bids[0:step*i])
+        self.means=[]
+        for i in range(1,101):
+            self.means[step*(i-1):step*i] = user.demand_curve(user.prices) * np.repeat(user.click_vs_bid(bids[i-1]),step)
+        self.sigmas = np.ones(len(actions)) * sigma 
 
     def round(self, pulled_arm):
         '''Simulate the current round of bidding with the given pulled arm. Returns the realization of a random normal with set mean and std.'''
