Fix step 1 variance

Environments/Environment.py
@@ -3,15 +3,21 @@ from Environments.Users import *
 
 
 class Environment:
+    
     avg_n_users = 100
     n_rounds = 365
+    selected_bid = 0.5
+
     def __init__(self, n_arms = 5, user = UserC1()):
         self.n_arms = n_arms  # 5 prices
         self.user = user
+        self.n_clicks = self.user.click_vs_bid(Environment.selected_bid)
 
     def round(self, pulled_arm):
         # Observation are communicated at the end of the day for all the customers
-        selected_bid = 0.5
-        n_clicks = self.user.click_vs_bid(selected_bid)
-        reward = np.random.binomial(n_clicks, self.user.probabilities[pulled_arm])/n_clicks
+        reward = np.random.binomial(self.n_clicks, self.user.probabilities[pulled_arm])/self.n_clicks
         return reward
+    
+    def max_reward(self):
+        # Return the maximum reward given the selected bid
+        return Environment.avg_n_users * np.max(self.user.reward_of_prices)

step_1.py
@@ -26,7 +26,7 @@ opt = max((Collector.prices-production_cost)*Collector.probabilities*n_clicks - 
 
 T = 365
 
-n_experiments = 1000
+n_experiments = 100
 ts_rewards_per_experiment = []
 gr_rewards_per_experiment = []
 ucb1_rewards_per_experiment = []
@@ -38,11 +38,6 @@ for e in range(0, n_experiments):
     gr_learner = Greedy_Learner(n_arms=n_arm, production_cost=production_cost, n_clicks=n_clicks, cumulative_cost=cumulative_cost)
     ucb1_learner = UCB1_Learner(n_arms=n_arm, production_cost=production_cost, n_clicks=n_clicks, cumulative_cost=cumulative_cost, M = Collector._max_price)
     for t in range(0, T):
-        # Thompson Sampling Learner
-        pulled_arm = ts_learner.pull_arm(Collector.prices)
-        reward = env.round(pulled_arm)
-        ts_learner.update(pulled_arm, reward, Collector.prices[pulled_arm])
-
         # Greedy Learner
         pulled_arm = gr_learner.pull_arm()
         reward = env.round(pulled_arm)
@@ -53,119 +48,124 @@ for e in range(0, n_experiments):
         reward = env.round(pulled_arm)
         ucb1_learner.update(pulled_arm, reward, Collector.prices[pulled_arm])
 
+        # Thompson Sampling Learner
+        pulled_arm = ts_learner.pull_arm(Collector.prices)
+        reward = env.round(pulled_arm)
+        ts_learner.update(pulled_arm, reward, Collector.prices[pulled_arm])
+
+
     gr_rewards_per_experiment.append(gr_learner.collected_rewards)
     ucb1_rewards_per_experiment.append(ucb1_learner.collected_rewards)
     ts_rewards_per_experiment.append(ts_learner.collected_rewards)
 
 # %% Compute the regret
-avg_regret_gr = np.mean(opt - gr_rewards_per_experiment, axis=0)
-avg_regret_ucb1 = np.mean(opt - ucb1_rewards_per_experiment, axis=0)
-avg_regret_ts = np.mean(opt - ts_rewards_per_experiment, axis=0)
+# opt = env.max_reward()  # TODO implement this
 
-std_regret_gr = np.std(opt - gr_rewards_per_experiment, axis=0)
-std_regret_ucb1 = np.std(opt - ucb1_rewards_per_experiment, axis=0)
-std_regret_ts = np.std(opt - ts_rewards_per_experiment, axis=0)
+regret_greedy = opt - gr_rewards_per_experiment  # row = exp, col = t
+avg_regret_greedy = np.mean(regret_greedy, axis=0)
+std_regret_greedy = np.std(regret_greedy, axis=0)
+cum_avg_regret_greedy = np.mean(np.cumsum(regret_greedy, axis=1), axis=0)
+cum_std_regret_greedy = np.std(np.cumsum(regret_greedy, axis=1), axis=0)
 
+regret_ucb1 = opt - ucb1_rewards_per_experiment  # row = exp, col = t
+avg_regret_ucb1 = np.mean(regret_ucb1, axis=0)
+std_regret_ucb1 = np.std(regret_ucb1, axis=0)
+cum_avg_regret_ucb1 = np.mean(np.cumsum(regret_ucb1, axis=1), axis=0)
+cum_std_regret_ucb1 = np.std(np.cumsum(regret_ucb1, axis=1), axis=0)
 
-# %%
-# solo per capire cosa sta succedendo
-print(opt)
+regret_ts = opt - ts_rewards_per_experiment  # row = exp, col = t
+avg_regret_ts = np.mean(regret_ts, axis=0)
+std_regret_ts = np.std(regret_ts, axis=0)
+cum_avg_regret_ts = np.mean(np.cumsum(regret_ts, axis=1), axis=0)
+cum_std_regret_ts = np.std(np.cumsum(regret_ts, axis=1), axis=0)
 
-print(np.shape(ucb1_rewards_per_experiment))
-print(ucb1_rewards_per_experiment[0])
-
-aux = opt - ucb1_rewards_per_experiment
-print(np.shape(aux))
-
-print(aux)
 
 # %% Plot the cumulative regret
-plt.figure(0)
+fig = plt.figure(0)
 plt.xlabel("t")
 plt.ylabel("Regret")
-plt.plot(np.cumsum(avg_regret_gr), 'g')
-plt.plot(np.cumsum(avg_regret_ucb1), 'b')
-plt.plot(np.cumsum(avg_regret_ts), 'r')
+plt.plot(cum_avg_regret_greedy, 'g')
+plt.plot(cum_avg_regret_ucb1, 'b')
+plt.plot(cum_avg_regret_ts, 'r')
+plt.fill_between(range(len(cum_avg_regret_greedy)), cum_avg_regret_greedy - cum_std_regret_greedy, cum_avg_regret_greedy + cum_std_regret_greedy, alpha=0.2, color='g')
+plt.fill_between(range(len(cum_avg_regret_ucb1)), cum_avg_regret_ucb1 - cum_std_regret_ucb1, cum_avg_regret_ucb1 + cum_std_regret_ucb1, alpha=0.2, color='b')
+plt.fill_between(range(len(cum_avg_regret_ts)), cum_avg_regret_ts - cum_std_regret_ts, cum_avg_regret_ts + cum_std_regret_ts, alpha=0.2, color='r')
 plt.legend(["Greedy", "UCB1", "TS"])
 plt.title("Cumulative Regret")
+fig = plt.gcf()
 plt.show()
 
-# %% Plot the cumulative reward
-plt.figure(1)
-plt.xlabel("t")
-plt.ylabel("Reward")
-plt.plot(np.cumsum(np.mean(gr_rewards_per_experiment, axis=0)), 'g')
-plt.plot(np.cumsum(np.mean(ucb1_rewards_per_experiment, axis=0)), 'b')
-plt.plot(np.cumsum(np.mean(ts_rewards_per_experiment, axis=0)), 'r')
-plt.legend(["Greedy", "UCB1", "TS"])
-plt.title("Cumulative Reward")
-plt.show()
+fig.savefig("results/S1_cumulative_regret.png")
 
 # %% Plot the instantaneous regret
-plt.figure(2)
+fig = plt.figure(1)
 plt.xlabel("t")
 plt.ylabel("Regret")
-plt.plot(avg_regret_gr, 'g')
+plt.plot(avg_regret_greedy, 'g')
 plt.plot(avg_regret_ucb1, 'b')
 plt.plot(avg_regret_ts, 'r')
-plt.hlines(y=0, xmin=0, xmax=T, colors='k', linestyles='dashed')
-#plt.ylim(-0.1, 1)
-plt.legend(["Greedy", "UCB1", "TS", "Clairvoyant"])
+plt.fill_between(range(len(avg_regret_greedy)), avg_regret_greedy - std_regret_greedy, avg_regret_greedy + std_regret_greedy, alpha=0.2, color='g')
+plt.fill_between(range(len(avg_regret_ucb1)), avg_regret_ucb1 - std_regret_ucb1, avg_regret_ucb1 + std_regret_ucb1, alpha=0.2, color='b')
+plt.fill_between(range(len(avg_regret_ts)), avg_regret_ts - std_regret_ts, avg_regret_ts + std_regret_ts, alpha=0.2, color='r')
+plt.hlines(0, 0, T, colors='black', linestyles='dashed')
+plt.legend(["Greedy", "UCB1", "TS"])
 plt.title("Instantaneous Regret")
+fig = plt.gcf()
 plt.show()
+
+fig.savefig("results/S1_instantaneous_regret.png")
+
+# %% Compute the reward
+avg_reward_greedy = np.mean(gr_rewards_per_experiment, axis=0)
+std_reward_greedy = np.std(gr_rewards_per_experiment, axis=0)
+cum_avg_reward_greedy = np.mean(np.cumsum(gr_rewards_per_experiment, axis=1), axis=0)
+cum_std_rreward_greedy = np.std(np.cumsum(gr_rewards_per_experiment, axis=1), axis=0)
+
+avg_reward_ucb1 = np.mean(ucb1_rewards_per_experiment, axis=0)
+std_reward_ucb1 = np.std(ucb1_rewards_per_experiment, axis=0)
+cum_avg_reward_ucb1 = np.mean(np.cumsum(ucb1_rewards_per_experiment, axis=1), axis=0)
+cum_std_reward_ucb1 = np.std(np.cumsum(ucb1_rewards_per_experiment, axis=1), axis=0)
+
+avg_reward_ts = np.mean(ts_rewards_per_experiment, axis=0)
+std_reward_ts = np.std(ts_rewards_per_experiment, axis=0)
+cum_avg_reward_ts = np.mean(np.cumsum(ts_rewards_per_experiment, axis=1), axis=0)
+cum_std_rreward_ts = np.std(np.cumsum(ts_rewards_per_experiment, axis=1), axis=0)
+
+
+# %% Plot the cumulative reward
+plt.figure(2)
+plt.xlabel("t")
+plt.ylabel("Reward")
+plt.plot(cum_avg_reward_greedy, 'g')
+plt.plot(cum_avg_reward_ucb1, 'b')
+plt.plot(cum_avg_reward_ts, 'r')
+plt.fill_between(range(len(cum_avg_reward_greedy)), cum_avg_reward_greedy - cum_std_rreward_greedy, cum_avg_reward_greedy + cum_std_rreward_greedy, alpha=0.2, color='g')
+plt.fill_between(range(len(cum_avg_reward_ucb1)), cum_avg_reward_ucb1 - cum_std_reward_ucb1, cum_avg_reward_ucb1 + cum_std_reward_ucb1, alpha=0.2, color='r')
+plt.fill_between(range(len(cum_avg_reward_ts)), cum_avg_reward_ts - cum_std_rreward_ts, cum_avg_reward_ts + cum_std_rreward_ts, alpha=0.2, color='b')
+plt.legend(["Greedy", "UCB1", "TS"])
+plt.title("Cumulative Reward")
+fig = plt.gcf()
+plt.show()
+
+fig.savefig("results/S1_cumulative_reward.png")
 
 # %% Plot the instantaneous reward
 plt.figure(3)
 plt.xlabel("t")
 plt.ylabel("Reward")
-plt.plot(np.mean(gr_rewards_per_experiment, axis=0), 'g')
-plt.plot(np.mean(ucb1_rewards_per_experiment, axis=0), 'b')
-plt.plot(np.mean(ts_rewards_per_experiment, axis=0), 'r')
+plt.plot(avg_reward_greedy, 'g')
+plt.plot(avg_reward_ucb1, 'b')
+plt.plot(avg_reward_ts, 'r')
 plt.hlines(y=opt, xmin=0, xmax=T, colors='k', linestyles='dashed')
+plt.fill_between(range(len(avg_reward_greedy)), avg_reward_greedy - std_reward_greedy, avg_reward_greedy + std_reward_greedy, alpha=0.2, color='g')
+plt.fill_between(range(len(avg_reward_ucb1)), avg_reward_ucb1 - std_reward_ucb1, avg_reward_ucb1 + std_reward_ucb1, alpha=0.2, color='b')
+plt.fill_between(range(len(avg_reward_ts)), avg_reward_ts - std_reward_ts, avg_reward_ts + std_reward_ts, alpha=0.2, color='r')
 plt.legend(["Greedy", "UCB1", "TS", "Clairvoyant"])
 plt.title("Instantaneous Reward")
+fig = plt.gcf()
 plt.show()
 
-
-# %% Plot the instantaneous regret with standard deviation
-plt.figure(4)
-plt.xlabel("t")
-plt.ylabel("Regret")
-plt.plot(avg_regret_gr, 'g')
-plt.plot(avg_regret_ucb1, 'b')
-plt.plot(avg_regret_ts, 'r')
-plt.hlines(y=0, xmin=0, xmax=T, colors='k', linestyles='dashed')
-plt.fill_between(range(len(avg_regret_gr)), avg_regret_gr - std_regret_gr, avg_regret_gr + std_regret_gr, color='g', alpha=0.2)
-plt.fill_between(range(len(avg_regret_ucb1)), avg_regret_ucb1 - std_regret_ucb1, avg_regret_ucb1 + std_regret_ucb1, color='b', alpha=0.2)
-plt.fill_between(range(len(avg_regret_ts)), avg_regret_ts - std_regret_ts, avg_regret_ts + std_regret_ts, color='r', alpha=0.2)
-plt.legend(["Greedy", "UCB1", "TS", "Clairvoyant"])
-plt.title("Instantaneous Regret with Standard Deviation")
-plt.show()
-
-
-
-
-# %% Plot of cumulative regret with variance
-avg_cum_regret_gr = np.cumsum(avg_regret_gr)
-avg_cum_regret_ucb1 = np.cumsum(avg_regret_ucb1)
-avg_cum_regret_ts = np.cumsum(avg_regret_ts)
-
-std_cum_regret_gr = np.cumsum(std_regret_gr)
-std_cum_regret_ucb1 = np.cumsum(std_regret_ucb1)
-std_cum_regret_ts = np.cumsum(std_regret_ts)
-
-plt.figure(1)
-plt.xlabel("t")
-plt.ylabel("Regret")
-plt.plot(avg_cum_regret_gr, 'g')
-plt.plot(avg_cum_regret_ucb1, 'b')
-plt.plot(avg_cum_regret_ts, 'r')
-plt.fill_between(range(len(avg_cum_regret_gr)), avg_cum_regret_gr - std_cum_regret_gr, avg_cum_regret_gr + std_cum_regret_gr, alpha=0.2, color='g')
-plt.fill_between(range(len(avg_cum_regret_ucb1)), avg_cum_regret_ucb1 - std_cum_regret_ucb1, avg_cum_regret_ucb1 + std_cum_regret_ucb1, alpha=0.2, color='b')
-plt.fill_between(range(len(avg_cum_regret_ts)), avg_cum_regret_ts - std_cum_regret_ts, avg_cum_regret_ts + std_cum_regret_ts, alpha=0.2, color='r')
-plt.legend(["Greedy", "UCB1", "TS"])
-plt.title("Cumulative Regret with standard deviation")
-plt.show()
+fig.savefig("results/S1_instantaneous_reward.png")
 
 
 # %%

step_2.py
@@ -55,17 +55,17 @@ for e in range(0, n_experiments):
 
 # %% Compute the regret
 opt = np.max(env.means)
-regret_gpts = opt - gpts_rewards_per_experiment  # row = exp, col = t
-avg_regret_gpts = np.mean(regret_gpts, axis=0)
-std_regret_gpts = np.std(regret_gpts, axis=0)
-cum_avg_regret_gpts = np.mean(np.cumsum(regret_gpts, axis=1), axis=0)
-cum_std_regret_gpts = np.std(np.cumsum(regret_gpts, axis=1), axis=0)
-
 regret_gpucb1 = opt - gpucb1_rewards_per_experiment  # row = exp, col = t
 avg_regret_gpucb1 = np.mean(regret_gpucb1, axis=0)
 std_regret_gpucb1 = np.std(regret_gpucb1, axis=0)
 cum_avg_regret_gpucb1 = np.mean(np.cumsum(regret_gpucb1, axis=1), axis=0)
 cum_std_regret_gpucb1 = np.std(np.cumsum(regret_gpucb1, axis=1), axis=0)
+
+regret_gpts = opt - gpts_rewards_per_experiment  # row = exp, col = t
+avg_regret_gpts = np.mean(regret_gpts, axis=0)
+std_regret_gpts = np.std(regret_gpts, axis=0)
+cum_avg_regret_gpts = np.mean(np.cumsum(regret_gpts, axis=1), axis=0)
+cum_std_regret_gpts = np.std(np.cumsum(regret_gpts, axis=1), axis=0)
 
 # %% Plot the cumulative regret
 fig = plt.figure(0)
@@ -99,15 +99,15 @@ plt.show()
 fig.savefig("results/S2_instantaneous_regret.png")
 
 # %% Compute the reward
-avg_reward_gpts = np.mean(gpts_rewards_per_experiment, axis=0)
-std_reward_gpts = np.std(gpts_rewards_per_experiment, axis=0)
-cum_avg_reward_gpts = np.mean(np.cumsum(gpts_rewards_per_experiment, axis=1), axis=0)
-cum_std_rreward_gpts = np.std(np.cumsum(gpts_rewards_per_experiment, axis=1), axis=0)
-
 avg_reward_gpucb1 = np.mean(gpucb1_rewards_per_experiment, axis=0)
 std_reward_gpucb1 = np.std(gpucb1_rewards_per_experiment, axis=0)
 cum_avg_reward_gpucb1 = np.mean(np.cumsum(gpucb1_rewards_per_experiment, axis=1), axis=0)
 cum_std_reward_gpucb1 = np.std(np.cumsum(gpucb1_rewards_per_experiment, axis=1), axis=0)
+
+avg_reward_gpts = np.mean(gpts_rewards_per_experiment, axis=0)
+std_reward_gpts = np.std(gpts_rewards_per_experiment, axis=0)
+cum_avg_reward_gpts = np.mean(np.cumsum(gpts_rewards_per_experiment, axis=1), axis=0)
+cum_std_rreward_gpts = np.std(np.cumsum(gpts_rewards_per_experiment, axis=1), axis=0)
 
 # %% Plot the cumulative reward
 plt.figure(2)