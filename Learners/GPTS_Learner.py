Fix variance and kernel in step 2

Learners/GPTS_Learner.py
@@ -18,11 +18,11 @@ class GPTS_Learner(Learner):
         self.sigmas = np.ones(self.n_arms) * 10.0
         self.pulled_arms = []
         alpha = 0.5  # alpha
-
         # The kernel is set as the product of a constant and a Radial-basis with values 1 and range 1e-3 to 1e3
         theta = 1.0
         l = 1.0
         kernel = C(theta, (1e-3, 1e3)) * RBF(l, (1e-3, 1e3))
+        # kernel = C(theta, constant_value_bounds="fixed") * RBF(l, length_scale_bounds="fixed") # This works way better
         n_restarts = 9
 
         # Sets the Gaussian Process Regressor from the given kernel

step_2.py
@@ -10,15 +10,18 @@ from Learners.GPUCB1_Learner import GPUCB1_Learner
 #%load_ext autoreload
 #%autoreload 2
 
+# TODO use scipy std since consider element as sample from population
+# TODO refactor reward 
+
 # %% Parameters
 n_arms = 100  #number of possible bids
 min_bid = 0.0
 max_bid = 1.0
 bids = np.linspace(min_bid, max_bid, n_arms)
-sigma = 10
+sigma = 10  # Noise of the environment TODO consider using %
 
-T = 365
-n_experiments = 4
+T = 60
+n_experiments = 20
 gpts_rewards_per_experiment = []
 gpucb1_rewards_per_experiment = []
 
@@ -52,35 +55,28 @@ for e in range(0, n_experiments):
 
 # %% Compute the regret
 opt = np.max(env.means)
-# opt = np.max([np.max(gpts_rewards_per_experiment,axis=1), np.max(gpucb1_rewards_per_experiment, axis=1)])
-#opt = np.array(120)
-avg_regret_gpts = np.mean(opt - gpts_rewards_per_experiment, axis=0)
-cum_regret_gpts = np.cumsum(avg_regret_gpts)
-std_regret_gpts = np.std(opt - gpts_rewards_per_experiment, axis=0)
-avg_regret_gpucb1 = np.mean(opt - gpucb1_rewards_per_experiment, axis=0)
-cum_regret_gpucb1 = np.cumsum(avg_regret_gpucb1)
-std_regret_gpucb1 = np.std(opt - gpucb1_rewards_per_experiment, axis=0)
+regret_gpts = opt - gpts_rewards_per_experiment  # row = exp, col = t
+avg_regret_gpts = np.mean(regret_gpts, axis=0)
+std_regret_gpts = np.std(regret_gpts, axis=0)
+cum_avg_regret_gpts = np.mean(np.cumsum(regret_gpts, axis=1), axis=0)
+cum_std_regret_gpts = np.std(np.cumsum(regret_gpts, axis=1), axis=0)
 
-# %%
-print(opt)
-
-print(np.shape(gpucb1_rewards_per_experiment))
-print(gpucb1_rewards_per_experiment[0])
-
-aux = opt - gpucb1_rewards_per_experiment
-print(np.shape(aux))
-
-print(aux)
+regret_gpucb1 = opt - gpucb1_rewards_per_experiment  # row = exp, col = t
+avg_regret_gpucb1 = np.mean(regret_gpucb1, axis=0)
+std_regret_gpucb1 = np.std(regret_gpucb1, axis=0)
+cum_avg_regret_gpucb1 = np.mean(np.cumsum(regret_gpucb1, axis=1), axis=0)
+cum_std_regret_gpucb1 = np.std(np.cumsum(regret_gpucb1, axis=1), axis=0)
 
 # %% Plot the cumulative regret
 fig = plt.figure(0)
 plt.xlabel("t")
 plt.ylabel("Regret")
-plt.plot(cum_regret_gpucb1, 'b')
-plt.plot(cum_regret_gpts, 'r')
-plt.fill_between(range(len(cum_regret_gpucb1)), cum_regret_gpucb1 - np.cumsum(std_regret_gpucb1), cum_regret_gpucb1 + np.cumsum(std_regret_gpucb1), alpha=0.2, color='b')
-plt.fill_between(range(len(cum_regret_gpts)), cum_regret_gpts - np.cumsum(std_regret_gpts), cum_regret_gpts + np.cumsum(std_regret_gpts), alpha=0.2, color='r')
+plt.plot(cum_avg_regret_gpucb1, 'b')
+plt.plot(cum_avg_regret_gpts, 'r')
+plt.fill_between(range(len(cum_avg_regret_gpucb1)), cum_avg_regret_gpucb1 - cum_std_regret_gpucb1, cum_avg_regret_gpucb1 + cum_std_regret_gpucb1, alpha=0.2, color='b')
+plt.fill_between(range(len(cum_avg_regret_gpts)), cum_avg_regret_gpts - cum_std_regret_gpts, cum_avg_regret_gpts + cum_std_regret_gpts, alpha=0.2, color='r')
 plt.legend(["GP-UCB1", "GPTS"])
+plt.title("Cumulative Regret")
 fig = plt.gcf()
 plt.show()
 
@@ -96,62 +92,51 @@ plt.fill_between(range(len(avg_regret_gpucb1)), avg_regret_gpucb1 - std_regret_g
 plt.fill_between(range(len(avg_regret_gpts)), avg_regret_gpts - std_regret_gpts, avg_regret_gpts + std_regret_gpts, alpha=0.2, color='r')
 plt.hlines(0, 0, T, colors='black', linestyles='dashed')
 plt.legend(["GP-UCB1", "GPTS"])
+plt.title("Instantaneous Regret")
 fig = plt.gcf()
 plt.show()
 
 fig.savefig("results/S2_instantaneous_regret.png")
 
+# %% Compute the reward
+avg_reward_gpts = np.mean(gpts_rewards_per_experiment, axis=0)
+std_reward_gpts = np.std(gpts_rewards_per_experiment, axis=0)
+cum_avg_reward_gpts = np.mean(np.cumsum(gpts_rewards_per_experiment, axis=1), axis=0)
+cum_std_rreward_gpts = np.std(np.cumsum(gpts_rewards_per_experiment, axis=1), axis=0)
+
+avg_reward_gpucb1 = np.mean(gpucb1_rewards_per_experiment, axis=0)
+std_reward_gpucb1 = np.std(gpucb1_rewards_per_experiment, axis=0)
+cum_avg_reward_gpucb1 = np.mean(np.cumsum(gpucb1_rewards_per_experiment, axis=1), axis=0)
+cum_std_reward_gpucb1 = np.std(np.cumsum(gpucb1_rewards_per_experiment, axis=1), axis=0)
+
 # %% Plot the cumulative reward
-plt.figure(1)
+plt.figure(2)
 plt.xlabel("t")
 plt.ylabel("Reward")
-plt.plot(np.cumsum(np.mean(gpucb1_rewards_per_experiment, axis=0)), 'b')
-plt.plot(np.cumsum(np.mean(gpts_rewards_per_experiment, axis=0)), 'r')
-plt.legend(["Greedy", "UCB1", "TS"])
+plt.plot(cum_avg_reward_gpucb1, 'b')
+plt.plot(cum_avg_reward_gpts, 'r')
+plt.fill_between(range(len(cum_avg_reward_gpucb1)), cum_avg_reward_gpucb1 - cum_std_reward_gpucb1, cum_avg_reward_gpucb1 + cum_std_reward_gpucb1, alpha=0.2, color='r')
+plt.fill_between(range(len(cum_avg_reward_gpts)), cum_avg_reward_gpts - cum_std_rreward_gpts, cum_avg_reward_gpts + cum_std_rreward_gpts, alpha=0.2, color='b')
+plt.legend(["GP-UCB1", "GPTS"])
 plt.title("Cumulative Reward")
+fig = plt.gcf()
 plt.show()
+
+fig.savefig("results/S2_cumulative_reward.png")
 
 # %% Plot the instantaneous reward
 plt.figure(3)
 plt.xlabel("t")
 plt.ylabel("Reward")
-plt.plot(np.mean(gpucb1_rewards_per_experiment, axis=0), 'b')
-plt.plot(np.mean(gpts_rewards_per_experiment, axis=0), 'r')
+plt.plot(avg_reward_gpucb1, 'b')
+plt.plot(avg_reward_gpts, 'r')
 plt.hlines(y=opt, xmin=0, xmax=T, colors='k', linestyles='dashed')
-plt.legend(["Greedy", "UCB1", "TS", "Clairvoyant"])
+plt.fill_between(range(len(avg_reward_gpucb1)), avg_reward_gpucb1 - std_reward_gpucb1, avg_reward_gpucb1 + std_reward_gpucb1, alpha=0.2, color='b')
+plt.fill_between(range(len(avg_reward_gpts)), avg_reward_gpts - std_reward_gpts, avg_reward_gpts + std_reward_gpts, alpha=0.2, color='r')
+plt.legend(["GP-UCB1", "GPTS",  "Clairvoyant"])
 plt.title("Instantaneous Reward")
+fig = plt.gcf()
 plt.show()
 
-# %% Plot the instantaneous regret with standard deviation
-plt.figure(4)
-plt.xlabel("t")
-plt.ylabel("Regret")
-plt.plot(avg_regret_gpucb1, 'b')
-plt.plot(avg_regret_gpts, 'r')
-plt.hlines(y=0, xmin=0, xmax=T, colors='k', linestyles='dashed')
-plt.fill_between(range(len(avg_regret_gpucb1)), avg_regret_gpucb1 - std_regret_gpucb1, avg_regret_gpucb1 + std_regret_gpucb1, color='b', alpha=0.2)
-plt.fill_between(range(len(avg_regret_gpts)), avg_regret_gpts - std_regret_gpts, avg_regret_gpts + std_regret_gpts, color='r', alpha=0.2)
-plt.legend(["Greedy", "UCB1", "TS", "Clairvoyant"])
-plt.title("Instantaneous Regret with Standard Deviation")
-plt.show()
-
-
-# %% Plot of cumulative regret with variance
-avg_cum_regret_ucb1 = np.cumsum(avg_regret_gpucb1)
-avg_cum_regret_ts = np.cumsum(avg_regret_gpts)
-
-std_cum_regret_ucb1 = np.cumsum(std_regret_gpucb1)
-std_cum_regret_ts = np.cumsum(std_regret_gpts)
-
-plt.figure(1)
-plt.xlabel("t")
-plt.ylabel("Regret")
-plt.plot(avg_cum_regret_ucb1, 'b')
-plt.plot(avg_cum_regret_ts, 'r')
-plt.fill_between(range(len(avg_cum_regret_ucb1)), avg_cum_regret_ucb1 - std_cum_regret_ucb1, avg_cum_regret_ucb1 + std_cum_regret_ucb1, alpha=0.2, color='b')
-plt.fill_between(range(len(avg_cum_regret_ts)), avg_cum_regret_ts - std_cum_regret_ts, avg_cum_regret_ts + std_cum_regret_ts, alpha=0.2, color='r')
-plt.legend(["Greedy", "UCB1", "TS"])
-plt.title("Cumulative Regret with standard deviation")
-plt.show()
-
+fig.savefig("results/S2_instantaneous_reward.png")
 # %%