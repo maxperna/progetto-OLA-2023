0bbdc237cf43abf5abf081bf9ebd786b5a306c9f
maxperna <maxperna@users.noreply.github.com>
2023-09-17 14:36:26 +0200

    Changed settings on parameters for contextual learners

--- Algorithm/Splitting_Context.py
+++ Algorithm/Splitting_Context.py
@@ -1,63 +1,74 @@
 import numpy as np
 from Environments.Users import UserC1,UserC2,UserC3
 from Environments.Context_environment import ContextEnvironment
+from Learners.GPTS_Contextual import Learner
 
 class Context():
 
     def __init__(self):
         self._possible_splits = [[0,0],[1,0],[0,1],[1,1]]
-        self._current_split = [0,0,0,0]
+        self._current_split = [False,False]   #Chech whether split on condition is happened or not
         self._all_split = False  #boolean to check if all the possible splits have been done
         self_first_split = True
         self._confidence_interval = 1e-5
 
-    def split_context_tree(self):
+    def split_context(self,context,learner):
         """
         Method used to split the context in two parts
         """
-        #TO BE IMPLEMENTED
-        feature_1_split = self._current_split[:2]
-        feature_2_split = self._current_split[2:]
-        #Complex split
-        feature_2_1_split = self._current_split[2:3]
-        feature_2_2_split = self._current_split[3:]
-        feature_1_1_split = self._current_split[:1]
-        feature_1_2_split = self._current_split[1:2]
+        #Create a map of the features of the context, maximum 3 splits
+        features_map = []
+        for splits in context.keys():
+            tmp = []
+            for user in context[splits]:
+                tmp.append(user.get_features)
+            features_map.append(tmp)    #created a map of feature for each context
 
+        feature_subset_1 = []
+        feature_subset_2 = []
+        new_context = []
+        for i in [0,1]:
+            splitting_feature = i
+            if not (self._current_split[splitting_feature]):
+                if len(new_context)!=0:
+                    features_map = new_context
+                for subcontext in features_map:
+                    for feature in subcontext:
+                        if feature[splitting_feature] == 0:
+                            feature_subset_1.append(feature)
+                        else:
+                            feature_subset_2.append(feature)
+
+
+                #Created the feature subset to split on
+                result = self.evaluate(learner,feature_subset_1,feature_subset_2)
+
+                if result:
+                    new_context = [feature_subset_1,feature_subset_2]
+                else:
+                    break
+
+    
 
         pass
-    def evaluate_splitting(self,rewards,users_collected):
+    def evaluate_splitting(self,learner: Learner,feature1,feature2):
         """
         Method used to evaluate the splitting of the users
         """
-        n_c1 = users_collected.count(1)
-        n_c2 = users_collected.count(2)
-        n_c3 = users_collected.count(3)
+        total_reward = np.array(learner.collected_rewards)
+        pulled_features = np.array(learner.features)
+        #Get the indices of the splitting features
+        indices_f1 = [index for index, value in enumerate(pulled_features) if value in feature1]
+        indices_f2 = [index for index, value in enumerate(pulled_features) if value in feature2]
 
-        total_user = len(users_collected)
+        reward_f1 = total_reward[indices_f1]
+        reward_f2 = total_reward[indices_f2]
 
-        for split in self._possible_splits:
-            if self._first_split:
-                # Divides C3 from the rest
-                feature_1_split = self._current_split[:2]
-                rewards_C3 = [value for value, num in zip(rewards, users_collected) if num == 3]
-                other_rew = [value for value, num in zip(rewards, users_collected) if num != 3]
-                prob_c3 = n_c3/total_user
-                prob_other = (total_user-n_c3)/total_user
-                if self.calculate_splitting_condition(prob_c3,prob_other,rewards_C3,other_rew,rewards):
-                    context = ContextEnvironment()
+        prob_1 = len(reward_f1)/len(total_reward)
+        prob_2 = len(reward_f2)/len(total_reward)
 
-                # Divides C3+C1 and C3+C2
-                feature_2_split = self._current_split[2:]
-                rewards_C3_C1 = [value for value, num in zip(rewards, users_collected) if num == 3 or num == 1]
-                prob_C3_C1 = (n_c3+n_c1)/total_user
-                rewards_C3_C2 = [value for value, num in zip(rewards, users_collected) if num == 3 or num == 2]
-                prob_C3_C2 = (n_c3+n_c2)/total_user
-                self.calculate_splitting_condition(prob_C3_C1, prob_C3_C1, rewards_C3_C2, prob_C3_C2, rewards)
+        return self.calculate_splitting_condition(prob_1,prob_2,reward_f1,reward_f2,total_reward)
 
-            #Splitting on a sub tree
-            else:
-                splits =
 
     def lower_bound(self,rewards):
         """
@@ -71,4 +82,3 @@ class Context():
             valid = True
         return valid
 
-    def fit_learner(self,reward):
